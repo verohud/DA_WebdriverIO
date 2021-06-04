56d181b9d8d17a6646f2068883e981a551b287c0
Veronika Hudakova <vero.hudakova@gmail.com>
2021-06-04 11:13:46 +0100

    added a new file to FinalTest folder

---
+++ FinalTest/GoogleTask.js
@@ -0,0 +1,79 @@
+
+// describe('Google Search Test', () => {
+
+//   it('should have the right title', (done) => {
+//   browser.url('https://google.com/')
+//   browser.pause(3000)
+//   search_box_element = $('[name=\'q\']')
+//   browser.pause(3000)
+//   search_box_element.setValue("BrowserStack\n")
+  
+//   expect(browser).toHaveTitle("BrowserStack - Google Search");
+
+//   browser.pause(3000)
+  
+//   //title of the launched webpage is printed in console as output
+  
+//   // const pagetitle = browser.getTitle()
+//   // console.log(‘Title of the webpage is - + pagetitle)
+//   })
+  
+//   })
+
+describe('Find Czechitas link on Google', () => {
+
+    it('Open Google, search the word Czechitas and click on the first link', ()=>{
+
+    browser.url('https://www.google.com/');
+
+      //agree with the Google's terms and conditions
+      let buttonAgree = $('#L2AGLb');        
+      buttonAgree.click();
+      // browser.pause(5000)
+
+      //search the word Czechitas and confirm
+      let searchBox = $('.gLFyf.gsfi');
+      searchBox.setValue('Czechitas');
+      let buttonSearch = $('.gNO89b[value="Google Search"]');
+      buttonSearch.click();
+      
+      
+      //click on the first link from the search results
+      let Czechitas = $('a .LC20lb');
+      Czechitas.click();
+      
+      //print the title of the website
+      let title = browser.getTitle();
+      console.log('Title is: ' + title);
+
+      //make sure the website url is containing the word czechitas
+      expect(browser).toHaveUrlContaining('czechitas');      
+        
+    });
+
+   });
+
+  // describe('V googlu vyhledám Czechitas', () => {
+  //   it('a otevřu první odkaz', () => {
+
+  //       //jde na stránku google.com
+  //       browser.url('http://www.google.com');
+
+  //       //klikne na souhlas s cookies
+  //       browser.$('#L2AGLb').click()
+
+  //       //do vyhledávacího okna zadá slovo Czechitas
+  //       let input = browser.$('.gLFyf.gsfi')
+  //               input.setValue('Czechitas');
+
+  //       //potvrdí entrem (chtěla jsem si zkusit, jestli to jde i jinak, než kliknutím)
+  //       browser.keys('\uE007')
+
+  //       //klikne na první odkaz (pro jistotu nereklamní - dvakrat se mi stalo, ze reklamni vedl na nejakou jinou stranku)
+  //       browser.$('.LC20lb.DKV0Md').click()
+  //       // browser.$('.cfxYMc.JfZTW.c4Djg.MUxGbd.v0nnCb').click() // varianta 2: klikne na prvni odkaz vcetne reklamnich
+  
+  //       //asertace, že jsem na správné stránce
+  //       let nadpisStranky = browser.$('.headline.perfo')
+  //       expect(nadpisStranky).toHaveTextContaining('IT je budoucnost.')
+  //   })})
